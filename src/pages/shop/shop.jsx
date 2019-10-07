c356308598ff93ddb98ba8a4b5604a33aa6df5a9
aituabdiluly <aituufs@gmail.com>
2019-10-07 21:07:23 +0200

    moved directory and shop data into its own reducer, created respective selectors and updated coresponding components with new redux flow for directory/shop

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Route } from "react-router-dom";
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.components';
-import CategoryPage from '../category/category.components';
+import CollectionPage from '../collection/collection.components';
 
 const ShopPage = ({match}) => (
     <div className="shop-page">
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:categoryID`} component={CategoryPage} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
