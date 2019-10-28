3b614db973331fbc430065ae7e389499cd70dabd
aituar <aituar@visualio.cz>
2019-10-28 18:38:13 +0100

    added WithSpinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyDxqXUNAB_DxYDjqVOY9VSJ9l1ivkWommw",
+  authDomain: "react-shop-9087d.firebaseapp.com",
+  databaseURL: "https://react-shop-9087d.firebaseio.com",
+  projectId: "react-shop-9087d",
+  storageBucket: "react-shop-9087d.appspot.com",
+  messagingSenderId: "151159914651",
+  appId: "1:151159914651:web:a7ecbc986f8256e17a463e"
 };
 
 firebase.initializeApp(config);
@@ -38,6 +38,40 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   return userRef;
 };
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+ 
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit(); 
+};
+
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+
+  const transformedCollection = collections.docs.map(doc => {
+    const {title, items} = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  })
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {})
+  
+}
+
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
